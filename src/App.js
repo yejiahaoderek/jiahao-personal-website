3f9ef6dc39002a095e2a3f25191e155b87ab1906
yejiahao <jyab2020@mymail.pomona.edu>
2022-02-09 20:23:44 -0800

    updated section

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import About from './Pages/About'
 import headshot from './picture/headshot.jpg';
 import meta from './picture/meta.png';
 import tiktok from './picture/tiktok.png';
+import gb from './picture/growthbox.png';
+import colby from './picture/colby.png';
 
 
 function App() {
   return (
     <div class=".bg-light.bg-gradient">
       <Header/>
-        <div className="alert alert-primary" role="alert">
+        <div class="alert alert-primary" role="alert">
             This site is still under construction!
         </div>
 
-      <section id="about" class=".bg-light.bg-gradient mt-4 mx-5 pt-4 px-4 pb-4">
+      <section id="about" class=".bg-light.bg-gradient mt-md-4 mx-md-5 pt-md-4 px-md-4 pb-md-4">
             <div class="mt-5 mx-5">
                 <img src={headshot} class="mb-4 headshot"/>
               <div class="fs-3">Hi 👋</div>
-                  <span class="fs-3">My name is <span class="fw-bold emphasize fs-2">Jiahao Ye</span><span class="fst-italic fs-4"> hi/him</span></span>
+                  <span class="fs-3">My name is <span class="fw-bold emphasize fs-2">Jiahao Ye</span><span class="fst-italic fs-4"> he/him</span></span>
                     <div class="fst-italic mb-2 mt-4">I'm a</div>
                     <div class="container d-flex flex-wrap">
                         <div className="tag">Photographer</div>
@@ -29,7 +31,7 @@ function App() {
                         <div className="tag">Cat lover</div>
                     </div>
             <div class="mt-4">
-                <p class="fs-8 lh-lg">I'm currently a senior at <a className="link" href="https://www.pomona.edu">Pomona
+                <p class="fs-8 fs-sm-1 lh-lg">I'm currently a senior at <a className="link" href="https://www.pomona.edu">Pomona
                     College</a> in California.</p>
                 <p class="fs-8 lh-lg">I'm from Deqing, China.</p>
               <p class="fs-8 lh-lg">I major in Computer Science. But I'm also very pasionate about social sciences. I study politics, international relations, anthropology, sociology, and had a field study in Uganda during my undergraduate studies.</p>
@@ -41,64 +43,78 @@ function App() {
 
         <br></br>
 
-      <section id="work" class=".bg-light.bg-gradient mt-4 mx-5 pt-4 px-4 pb-4">
+      <section id="work" class=".bg-light.bg-gradient mt-md-4 mx-md-5 pt-md-4 px-md-4 pb-md-4">
         <div className="mt-5 mx-5">
-            <h3 class="fw-bold">Experience</h3>
-            <div class="row">
-            <div class="col-12 col-lg-6">
-                <div class="fst-italic mt-4 mb-3">Current</div>
-                    <div class="container d-flex flex-wrap">
-                        <div class="role">
-                            <div>
-                                <span>Incoming Software Engineer Intern</span>
-                                <img src={meta} class="logo mx-4"/>
+            <h3 class="fw-bold mb-4">Experience</h3>
+                <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="fst-italic mb-3">Current</div>
+                        <div class="container d-flex flex-wrap">
+                            <div class="role">
+                                <div>
+                                    <span>Incoming Software Engineer Intern</span>
+                                </div>
+                            </div>
+                            <div className="role">
+                                Board Member @ 5C K-POP Performance Unit
+                            </div>
+                            <div className="role">
+                                Student Photographer @ Pomona Communications Office
+                            </div>
+                            <div className="role">
+                                Full stack developer @ Milk & Honey
                             </div>
                         </div>
-                        <div className="role">
-                            Board Member @ 5C K-POP Performance Unit
-                        </div>
-                        <div className="role">
-                            Student Photographer @ Pomona Communications Office
-                        </div>
-                        <div className="role">
-                            Full stack developer @ Milk & Honey
-                        </div>
-                    </div>
-            </div>
-            <div className="col-12 col-lg-6">
-                <div className="fst-italic mt-4 mb-3">Past</div>
+                </div>
+                <div className="col-12 col-md-6">
+                    <div className="fst-italic mt-4 mb-3">Past</div>
                     <div class="container d-flex flex-wrap">
                         <div class="role">
                             <div>
                                 <span>Product Strategy Intern</span>
-                                <img src={tiktok} className="logo mx-4"/>
                             </div>
                         </div>
-                        <div className="role">
-                            Admissions Tour Guide @ Colby College
+                        <div class="role">
+                            <div>
+                                <span>Growth Hacker Intern</span>
+                            </div>
                         </div>
-                        <div className="role">
+
+                        <div class="role row justify-content-center">
+                            <div class="col-md-4 col-12">Admissions Tour Guide</div>
+                        </div>
+                        <div class="role">
                             Student Videographer @ Colby Museum of Art
                         </div>
                     </div>
+                </div>
+                </div>
 
-
-            </div>
+            <div class="row mt-4">
+                    <div class="col-6 col-lg-3 d-flex flex-column"><img src={meta} class="logo align-self-center mt-4"/></div>
+                    <div class="col-6 col-lg-3 d-flex flex-column"><img src={tiktok} className="logo align-self-center mt-4"/></div>
+                    <div class="col-6 col-lg-3 d-flex flex-column"><img src={gb} className="logo align-self-center mt-4"/></div>
+                    <div class="col-6 col-lg-3 d-flex flex-column"><img src={colby} className="logo align-self-center mt-4"/></div>
             </div>
         </div>
+      </section>
+
+      <section id="cat" class=".bg-light.bg-gradient mt-md-4 mx-md-5 pt-md-4 px-md-4 pb-md-4">
+          <div class="mt-5 mx-5">
+              <h3 className="fw-bold mb-5">My Cat</h3>
+            <div className="embedsocial-hashtag" data-ref="95dc5f78c6fcc432b1a8b23adf85153ecee20ea7"></div>
+          </div>
 
       </section>
 
-      <section id="cat" class="border .bg-light.bg-gradient mt-4 mx-5 pt-4 px-4 pb-4">
-
+      <section id="photography" class=".bg-light.bg-gradient mt-md-4 mx-md-5 pt-md-4 px-md-4 pb-md-4">
+          <div className="mt-5 mx-5">
+          </div>
       </section>
 
-      <section id="photography" class="section .bg-light.bg-gradient mt-4 mx-5 pt-4 px-4 pb-4">
-
-      </section>
-
-      <section id="contact" class="section .bg-light.bg-gradient mt-4 mx-5 pt-5 pb-5 px-5 py-5">
-
+      <section id="contact" class=".bg-light.bg-gradient mt-md-4 mx-md-5 pt-md-4 px-md-4 pb-md-4">
+          <div className="mt-5 mx-5">
+          </div>
       </section>
 
 
--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     return ( 
         <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand fw-bolder fs-4 px-4" href="#">JY.</a>
+                <a class="navbar-brand fw-bolder fs-4 px-4" href="#">JiahaoY.</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
